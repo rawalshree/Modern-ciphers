05fbd14226183f5339ff90b6a6535af3a59e507b
rawalshree <rawalshreepal000@gmail.com>
2018-04-09 15:53:42 -0700

    main file for execution

--- cipher.py
+++ cipher.py
@@ -17,7 +17,6 @@ des = SimpleDES()
 def cipher(cipher_name, secret_key, enc_dec, input_file, output_file):
     intext = ""
     outtext = ""
-    chunks = []
     
     print("The cipher name is :", cipher_name)
     print("The secret key  is :", secret_key)
